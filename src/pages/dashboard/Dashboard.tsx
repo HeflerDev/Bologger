c2f4ec99e330318ce03076a40122027e41c4f3b0
s4uc3 <heflerdev@gmail.com>
2022-11-27 16:34:15 -0300

    Paginate Components

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -7,6 +7,7 @@ import IUserService from "../../services/UserService/IUserService";
 import UserService from "../../services/UserService";
 import {Posts, UserList} from "./components";
 import Post from "../../lib/Post";
+import ReactPaginate from "react-paginate";
 
 const Dashboard = (): JSX.Element => {
     const postService: IPostService = new PostService();
@@ -14,6 +15,11 @@ const Dashboard = (): JSX.Element => {
 
     const [posts, setPosts] = useState<Post[] | never>([])
     const [users, setUsers] = useState<User[] | never>([])
+
+    const [itemOffset, setItemOffset] = useState(0);
+    const endOffset = itemOffset + 10;
+    const currentItems = posts.slice(itemOffset, endOffset);
+    const pageCount = Math.ceil(posts.length / 10);
 
     useEffect(() => {
         (async () => {
@@ -24,15 +30,38 @@ const Dashboard = (): JSX.Element => {
         })();
     }, [])
 
+
+    const handlePageClick = (event: any) => {
+        const newOffset = (event.selected * 10) % posts.length;
+        setItemOffset(newOffset);
+        window.scrollTo(0, 0)
+    }
+
     return (
         <Stack>
             <Row>
                 <Col xs={12} lg={8}>
                     {
-                        posts && posts.map((post, index) => (
-                            <Posts post={post} index={index} key={post.title + index}/>
+                        posts && currentItems.map((post, index) => (
+                            <Posts post={post} index={(index + 1) * (itemOffset + 1)} key={post.title + index}/>
                         ))
                     }
+                    <ReactPaginate
+                        pageCount={pageCount}
+                        breakLabel={"  ...  "}
+                        onPageChange={handlePageClick}
+                        pageRangeDisplayed={1}
+                        previousLabel={"<"}
+                        nextLabel={">"}
+                        containerClassName={"paginate-container"}
+                        pageClassName={"paginate-page"}
+                        pageLinkClassName={"paginate-link"}
+                        previousClassName={"paginate__previous"}
+                        nextClassName={"paginate__next"}
+                        nextLinkClassName={"paginate-links__next"}
+                        previousLinkClassName={"paginate-links__previous"}
+                        activeLinkClassName={"paginate-active"}
+                    />
                 </Col>
                 <Col xs={12} md={6} lg={4}>
                     {
